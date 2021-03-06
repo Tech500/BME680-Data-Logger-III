cb2e9822b33eb8c7eea6892d977d797beacad5f9
William Lucid <11734452+Tech500@users.noreply.github.com>
2021-03-06 06:55:51 -0500

    Update index3.h

--- index3.h
+++ index3.h
@@ -1,4 +1,7 @@
 //index2.h
+
+// Add your "ThingSpeak.com" Iframes  ///////////////////////////////////////////////
+
 const char HTML3[] PROGMEM = R"====(
 <!DOCTYPE HTML>
 <html>
@@ -12,14 +15,14 @@ const char HTML3[] PROGMEM = R"====(
 	<h2>BME680 Observations</h2>
 	<br>
   <frameset rows="30%,70%" cols="33%,34%">
-  <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1320323/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=BME680+Temperature&type=line&xaxis=Date+and+Time&yaxis=Temperature+F."></iframe>
-  <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1320323/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=BME680+Humidity&type=line&xaxis=Date+and+Time&yaxis=Humidity"></iframe>
+  Your "ThingSpeak.com" Lfrsme goes here (one for each channel.) 
+  Your "ThingSpeak.com" Lfrsme goes here (one for each channel.)	
   </frameset>
   <br><br>
+  Your "ThingSpeak.com" Lfrsme goes here (one for each channel.)
+  Your "ThingSpeak.com" Lfrsme goes here (one for each channel.)
   <frameset rows="30%,70%" cols="33%,34%">
-  <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1320323/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=BME680+Pressure&type=line&xaxis=Date+and+Time&yaxis=Pressure+inHg."></iframe>
-  <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1320323/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=BME680+Gas&type=line&xaxis=Date+and+Time&yaxis=Gas+Res++KOhms"></iframe>
-  </frameset>
+    </frameset>
  	<br><br>
   <h2><a href="%LINK%/BME680" >Home</a></h2>
   <br>
